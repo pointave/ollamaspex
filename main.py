9a803e44b8dc4367e229e4b6eaf96dbbfb80cd13
point.aveugle <83179833+pointave@users.noreply.github.com>
2025-03-26 16:49:06 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
+from modules.screenshot_watcher import ScreenshotWatcher
+from modules.ui import ScreenshotAnalyzer
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    watcher = ScreenshotWatcher()
+
+    def on_screenshot_detected(image_path):
+        window = ScreenshotAnalyzer(image_path)
+        window.show()
+
+    watcher.screenshot_detected.connect(on_screenshot_detected)
+    watcher.start()
+
+    sys.exit(app.exec_())
